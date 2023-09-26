da00c530f1bbac5031ec6159691434cbf4c8a8cb
Kader Sarıkaya <74090818+kadersarikaya@users.noreply.github.com>
2023-09-26 16:53:14 +0300

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -8,7 +8,7 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
+      clientId: 682755774396-olsqbe21878r5r007jvqbnovvr630l4i.apps.googleusercontent.com,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     GithubProvider({
